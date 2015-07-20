7ca8ad53171963ff7e7e7a0492ffbd088b8bf64a
leseulsteve <leseulsteve@gmail.com>
2015-07-20 15:27:57 -0400

    init finction

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-'use strict'
+'use strict';
+
+var expressJwt = require('express-jwt');
 
 function UserAuth() {
+
 	return {
-		init = function(app, config) {
-			console.log(config);
+
+		init: function(app, config) {
+
+			app.use(config.apiUrl,
+				expressJwt({
+					secret: config.secret
+				}).unless({
+					path: app.config.unprotected
+				}));
 		}
 	}
 }
