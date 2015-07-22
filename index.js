6caa04c54d560b16fe5c003a78d8dc0513e7c691
leseulsteve <leseulsteve@gmail.com>
2015-07-21 20:38:20 -0400

    corrections

--- index.js
+++ index.js
@@ -3,41 +3,41 @@
 function UserAuth() {
 
 	var expressJwt = require('express-jwt'),
-		TokenService = require('./lib/services/token-service');
+		TokenService = require('./lib/services/token-service'),
+		_ = require('lodash-node');
+
+	var unless = function(path, middleware, unprotectedRoutes) {
+		return function(req, res, next) {
+			if (_.contains(unprotectedRoutes, req.path)) {
+				return next();
+			} else {
+				return middleware(req, res, next);
+			}
+		};
+	};
 
 	return {
 
 		init: function(app, UserSchema, MailTransporter, config) {
 
-			app.use(config.apiRoot,
-				expressJwt({
-					secret: config.token.secret
-				}).unless({
-					path: (config.unprotectedRoutes || []).concat([config.apiRoot + '/auth/signin', config.apiRoot + '/auth/send_password_token'])
-				})
-			);
-
-			app.use(config.apiRoot, function(err, req, res, next) {
-				if (!err) return next();
-				if (err.name === 'UnauthorizedError') {
-					res.status(401).send({
-						message: 'invalid token...'
-					});
-				}
-			});
-
 			TokenService.init(config.token);
+
+			var unprotectedRoutes = (config.unprotectedRoutes || []).concat([config.apiRoot + '/auth/signin', config.apiRoot + '/auth/send_password_token'])
+
+			app.use(unless('/' + config.apiRoot + '/*', TokenService.validate, unprotectedRoutes));
+			app.use(unless('/' + config.apiRoot + '/*', TokenService.injectUser, unprotectedRoutes));
 
 			var UserAuthController = require('./lib/controllers/user-auth')(UserSchema, MailTransporter, config);
 
-			app.route(config.apiRoot + '/auth/signin')
-      	.post(UserAuthController.signin);
+			app.route('/' + config.apiRoot + '/auth/signin')
+				.post(UserAuthController.signin);
 
-			app.route(config.apiRoot + '/auth/send_password_token')
-        .post(UserAuthController.sendPasswordToken);
+			app.route('/' + config.apiRoot + '/auth/send_password_token')
+				.post(UserAuthController.sendPasswordToken);
 
-      app.route(config.apiRoot + '/auth/change_passport')
-        .post(UserAuthController.changePassword);
+
+			app.route('/' + config.apiRoot + '/auth/change_passport')
+				.post(UserAuthController.changePassword);
 		},
 
 		getSecureUserSchema: function() {
