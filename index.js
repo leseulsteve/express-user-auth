3f72011d8147596e4612c2bb0dcbba3d68eba504
leseulsteve <leseulsteve@gmail.com>
2015-07-29 20:22:42 -0400

    corecitons

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 function UserAuth() {
 
 	var TokenService = require('./lib/services/token-service'),
-		_ = require('lodash-node'),
-		UserAuthController;
+		_ = require('lodash-node');
 
 	var unless = function(path, middleware, unprotectedRoutes) {
 		return function(req, res, next) {
-			if (_.contains(unprotectedRoutes, req.path)) {
+			if (_.contains(unprotectedRoutes, req.path.substr(1))) {
 				return next();
 			} else {
 				return middleware(req, res, next);
@@ -18,31 +17,53 @@ function UserAuth() {
 
 	return {
 
-		init: function(app, UserSchema, MailTransporter, config) {
+		init: function(app, UserSchema, config, MailTransporter) {
 
 			TokenService.init(config.token, UserSchema);
 
-			var unprotectedRoutes = (config.unprotectedRoutes || []).concat([config.apiRoot + '/auth/signin', config.apiRoot + '/auth/send_password_token' , config.apiRoot + '/auth/signup'])
+			var unprotectedRoutes = (config.unprotectedRoutes || []).concat([config.apiRoot + '/auth/signin', config.apiRoot + '/auth/send_password_token'])
+
+			if (config.signup && !config.signup.userCreationRoles) {
+				unprotectedRoutes.push(config.apiRoot + '/auth/signup');
+			}
 
 			app.use(unless('/' + config.apiRoot + '/*', TokenService.validate, unprotectedRoutes));
 			app.use(unless('/' + config.apiRoot + '/*', TokenService.injectUser, unprotectedRoutes));
 
-			UserAuthController = require('./lib/controllers/user-auth')(UserSchema, MailTransporter, config);
+			var UserAuthController = require('./lib/controllers/user-auth')(UserSchema, config, MailTransporter),
+				UserController = require('./lib/controllers/users')(UserSchema, config);
+
+			if (config.signup.sendConfirmationEmail) {
+				app.route('/' + config.apiRoot + '/auth/confirm_email')
+					.post(UserAuthController.confirmEmail);
+			}
+
+			if (MailTransporter) {
+				app.route('/' + config.apiRoot + '/auth/send_password_token')
+					.post(UserAuthController.sendPasswordToken);
+
+				app.route('/' + config.apiRoot + '/auth/change_passport')
+					.post(UserAuthController.changePassword);
+			}
 
 			app.route('/' + config.apiRoot + '/auth/signin')
 				.post(UserAuthController.signin);
 
-			app.route('/' + config.apiRoot + '/auth/send_password_token')
-				.post(UserAuthController.sendPasswordToken);
-
-			app.route('/' + config.apiRoot + '/auth/change_passport')
-				.post(UserAuthController.changePassword);
-
 			app.route('/' + config.apiRoot + '/auth/signup')
 				.post(UserAuthController.signup);
 
-			app.route('/' + config.apiRoot + '/auth/confirm_email')
-				.post(UserAuthController.confirmEmail);
+			app.route('/' + config.apiRoot + '/user')
+				.post(UserController.create)
+				.get(UserController.find);
+
+			app.route('/' + config.apiRoot + '/user/:userId')
+				//.all(ressourceAuthorization)
+				.get(UserController.findOne)
+				.put(UserController.update)
+				.delete(UserController.destroy);
+
+			app.param('userId', UserController.injectUser);
+
 		},
 
 		getSecureUserSchema: function() {
